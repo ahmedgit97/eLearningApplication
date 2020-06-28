58eeb4502ac0cd48089fcea91982eed0a6794cee
ahmedgit97 <ahmedhassan3841@gmail.com>
2020-06-28 23:03:31 +0500

    Initial commit

---
+++ src/test/java/com/example/eLearningApplication/ELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eLearningApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
